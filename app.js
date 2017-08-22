32eb254c36de869faac7a7033e1460fc1cdc71d6
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-08-22 21:22:44 +0800

    add lodash

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-console.log('Starting app.');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash')
+const notes = require('./notes.js');
 
-var user = os.userInfo();
+var res = notes.addNote();
+console.log(res);
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!\n`, function (err) {
-  if (err) {
-    console.log('Unable to write to file');
-  }
-});
+console.log('Result:', notes.add(3, -68));
+
+
+// var user = os.userInfo();
+//
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.\n`, function (err) {
+//   if (err) {
+//     console.log('Unable to write to file');
+//   }
+// });
